949f53f09f12ea36d7c5249cd7bc2a261876c785
adouch9 <mr.ouldmoussa@gmail.com>
2021-03-04 17:31:25 +0100

    calc

--- calc.js
+++ calc.js
@@ -2,3 +2,23 @@ const add = (nb1, nb2) => {
     return nb1 + nb2
 }
 
+const sub = (nb1, nb2) => {
+    return nb1 - nb2
+}
+const mul = (nb1, nb2) => {
+    return nb1 * nb2
+}
+const div = (nb1, nb2) => {
+    return nb1 / nb2
+}
+
+const calc = (op, nb1, nb2) => {
+    if(op === add || op === sub || op === mul || op === div){
+        return op(nb1, nb2)
+
+    }
+    
+
+}
+
+console.log(calc(mul,5,5)) 
